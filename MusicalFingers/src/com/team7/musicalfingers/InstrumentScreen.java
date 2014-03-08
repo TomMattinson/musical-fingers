162825c77636ae6906255684679280f0c648bb2b
Tabish <tabzraz@gmail.com>
2014-03-08 19:08:32 +0000

    First Commit

---
+++ MusicalFingers/src/com/team7/musicalfingers/InstrumentScreen.java
@@ -0,0 +1,36 @@
+package com.team7.musicalfingers;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class InstrumentScreen implements Screen {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void draw(SpriteBatch batch) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int changeStateTo() {
+		return -1;
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
